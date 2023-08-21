76df4086e130141135a1c8c3c819e26db8b758fd
Daniel Morales <danof.mora@gmail.com>
2023-08-20 20:51:32 -0700

    Finish playRound function

--- main.js
+++ main.js
@@ -4,4 +4,22 @@ function getComputerChoice () {
     return hand[Math.floor(Math.random() * hand.length)];
 } 
 
-alert (getComputerChoice());
+function playRound (playerSelection, computerSelection) {
+    console.log("computer chose " + computerSelection);
+    if (playerSelection === computerSelection)
+        return "Tie!";
+    else if (playerSelection === "rock" && computerSelection === "paper")
+        return "You lose! Paper beats Rock!";
+    else if (playerSelection === "rock" && computerSelection === "scissors")
+        return "You win! Rock beats Scissors!";
+    else if (playerSelection === "paper" && computerSelection === "rock")
+        return "You win! Paper beats Rock!";
+    else if (playerSelection === "paper" && computerSelection === "scissors")
+        return "You lose! Scissors beats Paper!";
+    else if (playerSelection === "scissors" && computerSelection === "paper")
+        return "You win! Scissors beats Paper!";
+    else 
+        return "You lose! Rock beats Scissors!";
+}
+
+alert (playRound("scissors", getComputerChoice()));
