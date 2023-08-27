2b6a3a80b402b1415733133e728d21a6f2ddfd1c
Daniel Morales <danny.mora542@gmail.com>
2023-08-26 21:17:06 -0700

    Added scoreboard and buttons

--- main.js
+++ main.js
@@ -1,30 +1,63 @@
-const hand = ["rock", "paper", "scissor"];
+const hand = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
     return hand[Math.floor(Math.random() * hand.length)];
 } 
 
+function setScore (winner) {
+    (winner === 'player') ? playerScore++ : computerScore ++
+    
+    const playerScoreBoard = document.querySelector('#player-score')
+    const computerScoreBoard = document.querySelector('#computer-score')
+
+    playerScoreBoard.textContent = 'Player Score: ' + playerScore;
+    computerScoreBoard.textContent = 'Computer Score: ' + computerScore;
+}
+
+
 function playRound (playerSelection, computerSelection) {
     console.log("computer chose " + computerSelection);
-    if (playerSelection === computerSelection)
+    if (playerSelection === computerSelection) {
         return "Tie!";
-    else if (playerSelection === "rock" && computerSelection === "paper")
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+        setScore('computer')
         return "You lose! Paper beats Rock!";
-    else if (playerSelection === "rock" && computerSelection === "scissors")
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        setScore('player')
         return "You win! Rock beats Scissors!";
-    else if (playerSelection === "paper" && computerSelection === "rock")
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        setScore('player')
         return "You win! Paper beats Rock!";
-    else if (playerSelection === "paper" && computerSelection === "scissors")
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        setScore('computer')
         return "You lose! Scissors beats Paper!";
-    else if (playerSelection === "scissors" && computerSelection === "paper")
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        setScore('player')
         return "You win! Scissors beats Paper!";
-    else 
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock"){ 
+        setScore('computer')
         return "You lose! Rock beats Scissors!";
+    }
 }
 
-let play = () => {
-    for (let i = 0; i < 5; i++) 
-        alert(playRound("rock", getComputerChoice()));
+function play(playerSelection) {
+    alert(playRound(playerSelection, getComputerChoice()))
 }
 
-play();
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+      play(button.id)
+    });
+  });
